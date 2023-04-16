00f97e479cdabd55eb4007e85a4a30995cd5c401
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-16 11:09:29 +0700

    update users table :rocket:

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('telephone', 15);
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
