2ebe76479fc253357faf5a64778f896997989f4e
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-16 11:16:05 +0700

    add user seeder :rocket:

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('1234567890'),
+            'telephone' => '083123456789',
+            'address' => 'Kuningan',
+        ]);
+
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('1234567890'),
+            'telephone' => '083123456789',
+            'address' => 'Kuningan',
+        ]);
+
+        $user->assignRole('user');
+    }
+}
