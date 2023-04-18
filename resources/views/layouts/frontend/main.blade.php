390122c1154417d1da30b1168b3d0b82905add08
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-18 15:54:04 +0700

    add main :rocket:

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -5,6 +5,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Rania Sport</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Premium Bootstrap 5 Landing Page Template" />
         <meta name="keywords" content="Saas, Software, multi-uses, HTML, Clean, Modern" />
