e7327a22ed11173775a3dab90700b071dec281eb
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-19 10:53:38 +0700

    add products views :rocket:

---
+++ resources/views/backend/products/add.blade.php
@@ -0,0 +1,243 @@
+@extends('layouts.backend.main')
+
+@section('title', 'Produk')
+
+@section('css')
+<!-- Datatables -->
+<link rel="stylesheet" href="{{ asset('backend') }}/libs/data-tables/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="{{ asset('backend') }}/libs/data-tables/css/responsive.bootstrap5.min.css">
+<link rel="stylesheet" href="{{ asset('backend') }}/libs/sweetalert2/sweetalert2.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" integrity="sha512-xmGTNt20S0t62wHLmQec2DauG9T+owP9e6VU8GigI0anN7OXLip9i7IwEhelasml2osdxX71XcYm6BQunTQeQg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="{{ asset('backend') }}/css/tag-input.css"/>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="layout-specing">
+        <div class="d-md-flex justify-content-between align-items-center">
+            <h5 class="mb-0">Produk</h5>
+
+            <nav aria-label="breadcrumb" class="d-inline-block">
+                <ul class="breadcrumb bg-transparent rounded mb-0 p-0">
+                    <li class="breadcrumb-item text-capitalize"><a href="{{ route('products.index') }}">Produk</a></li>
+                    <li class="breadcrumb-item text-capitalize active" aria-current="page">Tambah Data</li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="col-lg-12 mt-4">
+            <div class="card">
+                <div class="container">
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Foto <span class="text-danger">*</span></label>
+                                        <input name="image[]" id="image" type="file" class="form-control @error('image') is-invalid @enderror" multiple>
+                                        @error('image')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Nama Produk <span class="text-danger">*</span></label>
+                                        <input name="name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Nama Produk">
+                                        @error('name')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Kategori <span class="text-danger">*</span></label>
+                                        <select name="category_id" id="category_id" class="form-control @error('category_id') is-invalid @enderror">
+                                            <option value="">Pilih Kategori</option>
+                                            @foreach ($categories as $category)
+                                                <option id="categories_data" value="{{ $category->id }}"
+                                                    {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                                                    {{ $category->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                        @error('category_id')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Berat Barang <span class="text-danger">*</span></label>
+                                        <div class="row">
+                                            <div class="col-7 col-sm-8 mb-2">
+                                                <input name="weight" id="weight" type="number" class="form-control @error('weight') is-invalid @enderror" placeholder="Berat Barang">
+                                                @error('weight')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                            <div class="col-5 col-sm-4">
+                                                <select name="unit" id="unit" class="form-control @error('unit') is-invalid @enderror">
+                                                    <option value="">Pilih Satuan</option>
+                                                    <option value="g">g</option>
+                                                    <option value="kg">kg</option>
+                                                    <option value="ons">ons</option>
+                                                    <option value="pon">pon</option>
+                                                    <option value="lb">lb</option>
+                                                    <option value="oz">oz</option>
+                                                </select>
+                                            </div>
+                                            @error('unit')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Harga <span class="text-danger">*</span></label>
+                                        <input name="price" id="price" type="number" class="form-control @error('price') is-invalid @enderror" placeholder="Harga">
+                                        @error('price')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Stok <span class="text-danger">*</span></label>
+                                        <input name="stock" id="stock" type="number" class="form-control @error('stock') is-invalid @enderror" placeholder="Stok">
+                                        @error('stock')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Ukuran (Opsional)</label>
+                                        <input name="sizes" id="sizes" type="text" class="form-control" data-role="tagsinput">
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label">Warna (Opsional)</label>
+                                        <input name="colors" id="colors" type="text" class="form-control" data-role="tagsinput">
+                                    </div>
+                                </div><!--end col-->
+                                <div class="col-md-12">
+                                    <div class="mb-3">
+                                        <label class="form-label">Deskripsi Produk</label>
+                                        <textarea name="description" id="description" rows="4" class="form-control @error('description') is-invalid @enderror" placeholder="Deskripsi Produk"></textarea>
+                                        @error('description')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div><!--end row-->
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <input type="submit" id="submit" name="send" class="btn btn-primary" value="Send Message">
+                                </div><!--end col-->
+                            </div><!--end row-->
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end col-->
+    </div>
+</div><!--end container-->
+@endsection
+
+@section('javascript')
+<!-- Datatables -->
+<script src="{{ asset('backend') }}/libs/data-tables/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('backend') }}/libs/data-tables/js/dataTables.bootstrap5.min.js"></script>
+<script src="{{ asset('backend') }}/libs/data-tables/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('backend') }}/libs/data-tables/js/responsive.bootstrap5.min.js"></script>
+<script src="{{ asset('backend') }}/libs/sweetalert2/sweetalert2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js" integrity="sha512-9UR1ynHntZdqHnwXKTaOm1s6V9fExqejKvg5XMawEMToW4sSw+3jtLrYfZPijvnwnnE8Uol1O9BcAskoxgec+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    // show datatable with search and pagination
+    $(document).ready(function() {
+        $('#table').DataTable();
+    });
+
+    // function preview image
+    function previewImg() {
+        const logo = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+        const fileImg = new FileReader();
+        fileImg.readAsDataURL(logo.files[0]);
+        fileImg.onload = function(e) {
+            imgPreview.src = e.target.result;
+        }
+    }
+
+    // show dialog success
+    @if (Session::has('message'))
+        swal.fire({
+            icon: "success",
+            title: "Berhasil",
+            text: "{{ Session::get('message') }}",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                location.reload();
+            }
+        });
+    @endif
+
+    // function delete
+    $(".btn-delete").click(function() {
+        var id = $(this).data("id");
+        Swal.fire({
+            title: 'Hapus',
+            text: "Apakah anda yakin ingin menghapus?",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, Hapus!',
+            cancelButtonText: 'Batal',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: "products/" + id,
+                    type: 'DELETE',
+                    data: {
+                        "id": id,
+                        "_token": $('meta[name="csrf-token"]').attr('content'),
+                    },
+                    success: function(response) {
+                        swal.fire({
+                            icon: "success",
+                            title: "Berhasil",
+                            text: response.message,
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                location.reload();
+                            }
+                        });
+                    },
+                });
+            }
+        })
+    });
+
+</script>
+@endsection
---
+++ resources/views/backend/products/index.blade.php
@@ -0,0 +1,153 @@
+@extends('layouts.backend.main')
+
+@section('title', 'Produk')
+
+@section('css')
+<!-- Datatables -->
+<link rel="stylesheet" href="{{ asset('backend') }}/libs/data-tables/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="{{ asset('backend') }}/libs/data-tables/css/responsive.bootstrap5.min.css">
+<link rel="stylesheet" href="{{ asset('backend') }}/libs/sweetalert2/sweetalert2.min.css"/>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="layout-specing">
+        <div class="d-md-flex justify-content-between align-items-center">
+            <h5 class="mb-0">Produk</h5>
+
+            <nav aria-label="breadcrumb" class="d-inline-block">
+                <ul class="breadcrumb bg-transparent rounded mb-0 p-0">
+                    <li class="breadcrumb-item text-capitalize"><a href="#">Produk</a></li>
+                    <li class="breadcrumb-item text-capitalize active" aria-current="page">list</li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="row">
+            <div class="col-12 mt-4">
+                <div class="d-grid gap-2 d-md-flex">
+                    <a href="{{ route('products.create') }}" class="btn btn-primary mb-3 btn-sm">
+                        Tambah Data +
+                    </a>
+                </div>
+                <div class="table-responsive shadow rounded">
+                    <div class="card-body">
+                        <table class="table table-center bg-white mb-0" id="table">
+                            <thead>
+                                <tr>
+                                    <th class="text-center border-bottom p-3">No</th>
+                                    <th class="border-bottom p-3">Foto</th>
+                                    <th class="border-bottom p-3">Nama</th>
+                                    <th class="border-bottom p-3">Ukuran</th>
+                                    <th class="border-bottom p-3">Warna</th>
+                                    <th class="border-bottom p-3">Berat</th>
+                                    <th class="border-bottom p-3">Harga</th>
+                                    <th class="border-bottom p-3">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- Start -->
+                                @foreach($products as $product)
+                                    <tr>
+                                        <th class="text-center p-3" style="width: 5%;">{{ $loop->iteration }}</th>
+                                        <td class="p-3"><img src="{{ asset('storage/products/' . $product->image) }}" width="100px" class="img-fluid" alt="image-products"></td>
+                                        <td class="p-3">{{ $product->name }}</td>
+                                        <td class="p-3">{{ $product->sizes }}</td>
+                                        <td class="p-3">{{ $product->colors }}</td>
+                                        <td class="p-3">{{ $product->weight }}</td>
+                                        <td class="p-3">{{ $product->price }}</td>
+                                        <td style="width: 25%;">
+                                            <button type="button" class="btn btn-warning btn-sm mb-2" data-bs-toggle="modal" data-bs-target="#edit-modal{{ $product->id }}"><i class="fa-solid fa-pen"></i> Edit</button>
+                                            <button type="button" class="btn btn-danger btn-sm mb-2 btn-delete" data-id="{{ $product->id }}"><i class="fa-solid fa-trash"></i> Hapus</button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                <!-- End -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!--end col-->
+        </div>
+        <!--end row-->
+    </div>
+</div><!--end container-->
+@endsection
+
+@section('javascript')
+<!-- Datatables -->
+<script src="{{ asset('backend') }}/libs/data-tables/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('backend') }}/libs/data-tables/js/dataTables.bootstrap5.min.js"></script>
+<script src="{{ asset('backend') }}/libs/data-tables/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('backend') }}/libs/data-tables/js/responsive.bootstrap5.min.js"></script>
+<script src="{{ asset('backend') }}/libs/sweetalert2/sweetalert2.min.js"></script>
+
+<script>
+    // show datatable with search and pagination
+    $(document).ready(function() {
+        $('#table').DataTable();
+    });
+
+    // function preview image
+    function previewImg() {
+        const logo = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+        const fileImg = new FileReader();
+        fileImg.readAsDataURL(logo.files[0]);
+        fileImg.onload = function(e) {
+            imgPreview.src = e.target.result;
+        }
+    }
+
+    // show dialog success
+    @if (Session::has('message'))
+        swal.fire({
+            icon: "success",
+            title: "Berhasil",
+            text: "{{ Session::get('message') }}",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                location.reload();
+            }
+        });
+    @endif
+
+    // function delete
+    $(".btn-delete").click(function() {
+        var id = $(this).data("id");
+        Swal.fire({
+            title: 'Hapus',
+            text: "Apakah anda yakin ingin menghapus?",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, Hapus!',
+            cancelButtonText: 'Batal',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: "products/" + id,
+                    type: 'DELETE',
+                    data: {
+                        "id": id,
+                        "_token": $('meta[name="csrf-token"]').attr('content'),
+                    },
+                    success: function(response) {
+                        swal.fire({
+                            icon: "success",
+                            title: "Berhasil",
+                            text: response.message,
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                location.reload();
+                            }
+                        });
+                    },
+                });
+            }
+        })
+    });
+
+</script>
+@endsection
