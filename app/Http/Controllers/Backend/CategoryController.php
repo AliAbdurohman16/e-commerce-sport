5e43016663f154ffde25f47d88acb15d0b0855d4
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-19 11:30:29 +0700

    add comments to code :rocket:

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -12,6 +12,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
+        // get data
         $categories = Category::all();
 
         return view('backend.categories.index', compact('categories'));
@@ -19,11 +20,13 @@ class CategoryController extends Controller
 
     public function store(Request $request)
     {
+        // validation
         $request->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
             'name' => 'required|max:255',
         ]);
 
+        // process upload image
         if ($request->hasFile('image')) {
             $imagePath = $request->file('image')->store('public/categories');
             $imageName = basename($imagePath);
@@ -31,6 +34,7 @@ class CategoryController extends Controller
             $imageName = '';
         }
 
+        // insert to table categories
         Category::create([
             'image' => $imageName,
             'name' => $request->name,
@@ -42,13 +46,16 @@ class CategoryController extends Controller
 
     public function update(Request $request, $id)
     {
+        // validation
         $request->validate([
             'image' => 'image|mimes:jpeg,png,jpg|max:2048',
             'name' => 'required|max:255',
         ]);
 
+        // get data find or fail by id
         $category = Category::findOrFail($id);
 
+        // process upload image
         if ($request->hasFile('image')) {
             Storage::delete('public/categories/' . $category->image);
             $imagePath = $request->file('image')->store('public/categories');
@@ -57,6 +64,7 @@ class CategoryController extends Controller
             $imageName = $category->image;
         }
 
+        // update to table categories
         $category->update([
             'image' => $imageName,
             'name' => $request->name,
@@ -68,12 +76,15 @@ class CategoryController extends Controller
 
     public function destroy($id)
     {
+        // get data find or fail by id
         $category = Category::findOrFail($id);
 
+        // process delete image
         if ($category->image) {
             Storage::delete('public/categories/' . $category->image);
         }
 
+        // delete data
         $category->delete();
 
         return response()->json(['message' => 'Kategori berhasil dihapus!']);
