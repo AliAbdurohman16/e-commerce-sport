d448c84de50b7ff667b87c2d751c0b520fa21fa9
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-24 19:11:50 +0700

    update navbar :rocket:

--- resources/views/layouts/frontend/navbar.blade.php
+++ resources/views/layouts/frontend/navbar.blade.php
@@ -23,7 +23,7 @@
         </div>
 
         <ul class="buy-button list-inline mb-0">
-            <li class="list-inline-item mb-0 pe-1">
+            <li class="list-inline-item mb-0">
                 <div class="dropdown">
                     <button type="button" class="btn dropdown-toggle p-0" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="uil uil-search text-dark fs-5 align-middle"></i>
@@ -48,9 +48,62 @@
                 </li>
                 @endif
             @else
-                <li class="list-inline-item mb-0 pe-1">
+                <li class="list-inline-item mb-0 pe-1 position-relative">
                     <div class="dropdown">
-                        <button type="button" class="btn btn-icon btn-pills btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="shopping-cart" class="icons"></i></button>
+                        <button type="button" class="btn btn-icon btn-pills btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i data-feather="shopping-cart" class="icons"></i>
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">3</span>
+                        </button>
+                        <div class="dropdown-menu dd-menu dropdown-menu-end bg-white shadow rounded border-0 mt-3 p-4" style="width: 300px;">
+                            <div class="pb-4">
+                                <a href="javascript:void(0)" class="d-flex align-items-center">
+                                    <img src="{{ asset('frontend') }}/images/shop/product/s-1.jpg" class="shadow rounded" style="max-height: 64px;" alt="">
+                                    <div class="flex-1 text-start ms-3">
+                                        <h6 class="text-dark mb-0">T-shirt (M)</h6>
+                                        <p class="text-muted mb-0">$320 X 2</p>
+                                    </div>
+                                    <h6 class="text-dark mb-0">$640</h6>
+                                </a>
+
+                                <a href="javascript:void(0)" class="d-flex align-items-center mt-4">
+                                    <img src="{{ asset('frontend') }}/images/shop/product/s-2.jpg" class="shadow rounded" style="max-height: 64px;" alt="">
+                                    <div class="flex-1 text-start ms-3">
+                                        <h6 class="text-dark mb-0">Bag</h6>
+                                        <p class="text-muted mb-0">$50 X 5</p>
+                                    </div>
+                                    <h6 class="text-dark mb-0">$250</h6>
+                                </a>
+
+                                <a href="javascript:void(0)" class="d-flex align-items-center mt-4">
+                                    <img src="{{ asset('frontend') }}/images/shop/product/s-3.jpg" class="shadow rounded" style="max-height: 64px;" alt="">
+                                    <div class="flex-1 text-start ms-3">
+                                        <h6 class="text-dark mb-0">Watch (Men)</h6>
+                                        <p class="text-muted mb-0">$800 X 1</p>
+                                    </div>
+                                    <h6 class="text-dark mb-0">$800</h6>
+                                </a>
+                            </div>
+
+                            <div class="d-flex align-items-center justify-content-between pt-4 border-top">
+                                <h6 class="text-dark mb-0">Total($):</h6>
+                                <h6 class="text-dark mb-0">$1690</h6>
+                            </div>
+
+                            <div class="mt-3 text-center">
+                                <a href="javascript:void(0)" class="btn btn-primary me-2">View Cart</a>
+                                <a href="javascript:void(0)" class="btn btn-primary">Checkout</a>
+                            </div>
+                            <p class="text-muted text-start mt-1 mb-0">*T&C Apply</p>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="list-inline-item mb-0 pe-1 position-relative">
+                    <div class="dropdown">
+                        <button type="button" class="btn btn-icon btn-pills btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i data-feather="message-square" class="icons"></i>
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">3</span>
+                        </button>
                         <div class="dropdown-menu dd-menu dropdown-menu-end bg-white shadow rounded border-0 mt-3 p-4" style="width: 300px;">
                             <div class="pb-4">
                                 <a href="javascript:void(0)" class="d-flex align-items-center">
