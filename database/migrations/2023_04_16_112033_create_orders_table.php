f2ebda18b27a4591fded621378934aa9a62ede83
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-08-02 08:36:32 +0700

    adding field review to table orders :rocket:

--- database/migrations/2023_04_16_112033_create_orders_table.php
+++ database/migrations/2023_04_16_112033_create_orders_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('status')->default('Belum Checkout');
             $table->decimal('subtotal', 11, 2)->nullable();
+            $table->string('review', 4)->nullable();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
