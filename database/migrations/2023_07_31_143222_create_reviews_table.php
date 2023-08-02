0ae421c4cc410d3ed7322e7a5f36b3f1b71f8679
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-08-02 14:51:14 +0700

    adding product id and adding nullable for field comment :rocket:

--- database/migrations/2023_07_31_143222_create_reviews_table.php
+++ database/migrations/2023_07_31_143222_create_reviews_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id');
             $table->unsignedBigInteger('user_id');
             $table->string('rating');
-            $table->text('comment');
+            $table->text('comment')->nullable();
             $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
